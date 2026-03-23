c54e4b154c115b3f7719f00b497ee70869181591
ThenrajM <thenraj2743555@gmail.com>
2026-03-23 17:11:02 +0530

    After Branch

--- src/Demo/Sample1.java
+++ src/Demo/Sample1.java
@@ -4,8 +4,17 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		boolean isRaining = false;
  System.out.println("hello gokul");
  System.out.println("hello raj");
+ 
+ 
+
+if (isRaining) {
+  System.out.println("Bring an umbrella!");
+} else {
+  System.out.println("No rain today, no need for an umbrella!");
+}
 	}
 
 }
